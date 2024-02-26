c8e9c5dab87545b605ea4e8d4f258ea8874674ab
Priyanvitha <120250370+Priyanvitha@users.noreply.github.com>
2024-02-26 21:57:57 +0530

    Update 3.1.6.py
    3.1.10 completed

--- 3.1.6.py
+++ 3.1.6.py
@@ -1,2 +1,12 @@
 n = int(input("Enter a number: "))
 print(n >= 100)
+
+name = input("Enter flower name: ")
+
+if name == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif name == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print("Spathiphyllum! Not", name + "!")
+	
