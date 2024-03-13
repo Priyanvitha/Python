4092d1bb14da131dc01007c43046ab382ff1f6b2
Priyanvitha <=>
2024-03-13 18:55:08 +0530

    3.2.9 is completed

---
+++ 3.2.9.py
@@ -0,0 +1,8 @@
+#3.2.9   LAB   The break statement – Stuck in a loop
+
+
+while True:
+    user_input = input("Enter a word: ")
+    if user_input == "chupacabra":
+        print("You've successfully left the loop.")
+        break
