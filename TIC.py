d49c6481f494e5fd140ded9956bbcad00efed7f8
Priyanvitha <120250370+Priyanvitha@users.noreply.github.com>
2024-03-31 22:30:27 +0530

    Add files via upload

---
+++ TIC.py
@@ -0,0 +1,10 @@
+def make_list_of_free_fields(board):
+   free = [] 
+   for i in range(3) :
+        for j in range(3) :
+            if board[i][j] not in ["O","X"] :
+                free.append((i,j))
+   return free 
+
+
+
---
+++ tac.py
@@ -0,0 +1,7 @@
+def display_board():
+    print("+-----+-----+-----+")
+    for i in range(3) :
+        print("|     |     |     |\n|     |     |     |\n|     |     |     |")
+        print("+-----+-----+-----+")
+display_board() 
+    
